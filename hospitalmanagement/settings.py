4fea45d3e3178bc03e9b16f739230da046297226
anjalisingh17 <33294705+anjalisingh17@users.noreply.github.com>
2022-08-24 12:41:59 +0530

    updated settings.py

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -1,4 +1,6 @@
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -105,7 +107,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[STATIC_DIR,]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS= (os.path.join(BASE_DIR, 'static'))
 
+django_heroku.settings(local())
 
 
 LOGIN_REDIRECT_URL='/afterlogin'
